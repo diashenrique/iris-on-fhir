7fd6dbe163a40e7519521c27fc3de41ac3aa636e
jrpereirajr <ze.sjrp@gmail.com>
2021-05-27 23:12:56 -0300

    refactoring for isolate common code in commons.js

---
+++ src/csp/assets/js/commons.js
@@ -0,0 +1,27 @@
+// enable debug mode in SMART JS client lib
+localStorage.debug = "FHIR.*";
+
+function getFHIRClient() {
+  // return FHIR.oauth2.ready()
+  //     .then(client => client);
+  return expiredTokenWorkaround()
+    .then(client => client);
+}
+
+function expiredTokenWorkaround() {
+  return FHIR.oauth2.ready()
+    .then(client => {
+      const accessToken = client.state.tokenResponse.access_token;
+      const refreshToken = client.state.tokenResponse.refresh_token;
+      const expiresAt = client.state.expiresAt || 0;
+
+      if (!accessToken) {
+        window.location.pathname = "/iris-on-fhir/launch.html";
+      }
+      if (accessToken && refreshToken && expiresAt - 10 < Date.now() / 1000) {
+        window.location.pathname = "/iris-on-fhir/launch.html";
+      }
+
+      return client;
+    });
+}
