f7d4d5417837d3c95ccbe5fe31ee18d0afabee7d
Henrique Dias <henrique.dias@gmail.com>
2021-06-02 18:20:56 -0300

    Added encounters, immunization, claims to patient view

--- src/csp/assets/js/patientview.js
+++ src/csp/assets/js/patientview.js
@@ -1,12 +1,15 @@
 $(document).ready(function () {
+    generateRandomAvatar();
     getFHIRClient()
         .then(client => {
             getPatientDetails(client)
                 .then(bundle => {
-                    console.log("getFHIRClientBundle", bundle);
+                    // console.log("getFHIRClientBundle", bundle);
                     return;
                 })
                 .then(getEncounters)
+                .then(getImmunization)
+                .then(getClaimsByPatient)
         })
         .catch(console.error);
 });
@@ -63,9 +66,9 @@ function getEncounters() {
             return client.request(`Encounter?${query}`)
                 .then((bundle) => {
                     // console.log("getEncounters",bundle);
-                    const arrEncounter = bundle.entry.reverse();
+                    let arrEncounter = bundle.entry.reverse();
                     for (var i = 0; i < arrEncounter.length; i++) {
-                        console.log(i, arrEncounter[i]);
+                        // console.log(i, arrEncounter[i]);
                         let reasonText = "";
                         let dtPeriod = "";
                         let strServiceProvider = "";
@@ -76,7 +79,7 @@ function getEncounters() {
                             }
                         } else {
                             reasonText = arrEncounter[i].resource.reasonCode[0].coding[0].display;
-                            console.log(i, arrEncounter[i].resource.reasonCode[0].coding[0].display, arrEncounter[i].resource.period.start, arrEncounter[i].resource.serviceProvider.display);
+                            // console.log(i, arrEncounter[i].resource.reasonCode[0].coding[0].display, arrEncounter[i].resource.period.start, arrEncounter[i].resource.serviceProvider.display);
                         }
 
                         dtPeriod = (arrEncounter[i].resource.period.start).split("T")[0];
@@ -109,7 +112,121 @@ function getEncounters() {
         });
 }
 
-function parseISOString(s) {
-    var b = s.split(/\D+/);
-    return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+function getClaimsByPatient() {
+    return FHIR.oauth2.ready()
+        .then(client => {
+            const urlParameters = new URLSearchParams(window.location.search);
+            const patientId = urlParameters.get("pid");
+
+            const query = new URLSearchParams();
+            query.set("patient", `${patientId}`)
+            query.set("_sort", "created");
+            // query.set("_sort", "-_lastUpdated");
+
+            return client.request(`Claim?${query}`)
+                .then((bundle) => {
+                    $("#claim-dataTable").DataTable({
+                        "dom": '<"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
+                        "processing": true,
+                        "data": bundle.entry,
+                        "order": [
+                            [0, "desc"]
+                        ],
+                        "columns": [{
+                            "data": "resource.id"
+                        }, {
+                            "data": "resource.created",
+                            "render": function (data, type, row, meta) {
+                                return data.split("T")[0];
+                            }
+                        }, {
+                            "data": "resource.provider.display"
+                        }, {
+                            "data": "resource.total.currency"
+                        }, {
+                            "data": "resource.total.value",
+                            className: "text-right",
+                            render: $.fn.dataTable.render.number(',', '.', 2, '$')
+                        }, {
+                            "data": "resource.status"
+                        }]
+                    });
+                })
+                .catch((err) => {
+                    // Error responses
+                    if (err.status) {
+                        console.log(err);
+                        console.log('Error', err.status);
+                    }
+                    // Errors
+                    if (err.data && err.data) {
+                        console.log('Error', err.data);
+                    }
+                });
+        });
+}
+
+function getImmunization() {
+    return FHIR.oauth2.ready()
+        .then(client => {
+            const urlParameters = new URLSearchParams(window.location.search);
+            const patientId = urlParameters.get("pid");
+
+            const query = new URLSearchParams();
+            query.set("patient", `${patientId}`)
+            query.set("_sort", "-_id");
+            query.set("_count", 3);
+
+            return client.request(`Immunization?${query}`)
+                .then((bundle) => {
+                    console.log("getImmunization", bundle);
+                    let arrImmunization = bundle.entry.reverse();
+                    if (arrImmunization.length > 0) {
+                        for (var i = 0; i < arrImmunization.length; i++) {
+                            console.log(i, arrImmunization[i]);
+                            let dtImmunization = "";
+                            let strVaccine = "";
+                            let stImmunization = "";
+    
+                            dtImmunization = (arrImmunization[i].resource.occurrenceDateTime).split("T")[0];
+                            strVaccine = arrImmunization[i].resource.vaccineCode.text;
+                            stImmunization = arrImmunization[i].resource.status;
+    
+                            let liImmunization1 = '<li class="timeline-item"><span class="timeline-point timeline-point-indicator"></span><div class="timeline-event"><div class="d-flex justify-content-between flex-sm-row flex-column mb-sm-0 mb-1">'
+                            let liImmunization2 = `<h6>${strVaccine}</h6><span class="timeline-event-time">${dtImmunization}</span></div>`
+                            let liImmunization3 = `<p>${stImmunization}</p></div></li>`
+                            let liImmunization = liImmunization1.concat(liImmunization2).concat(liImmunization3);
+    
+                            $("#idImmunization").append(liImmunization);
+                        }
+                    } else {
+                        let liImmunization1 = '<li class="timeline-item"><span class="timeline-point timeline-point-indicator"></span><div class="timeline-event"><div class="d-flex justify-content-between flex-sm-row flex-column mb-sm-0 mb-1">'
+                        let liImmunization2 = `<h6></h6><span class="timeline-event-time"></span></div><p></p></div></li>`
+                        let liImmunization3 = '<li class="timeline-item"><span class="timeline-point timeline-point-indicator"></span><div class="timeline-event"><div class="d-flex justify-content-between flex-sm-row flex-column mb-sm-0 mb-1">'
+                        let liImmunization4 = `<h6>NO DATA</h6><span class="timeline-event-time"></span></div><p></p></div></li>`
+                        let liImmunization5 = '<li class="timeline-item"><span class="timeline-point timeline-point-indicator"></span><div class="timeline-event"><div class="d-flex justify-content-between flex-sm-row flex-column mb-sm-0 mb-1">'
+                        let liImmunization6 = `<h6></h6><span class="timeline-event-time"></span></div><p></p></div></li>`
+                        let liImmunization = liImmunization1.concat(liImmunization2).concat(liImmunization3).concat(liImmunization4).concat(liImmunization5).concat(liImmunization6);
+                        $("#idImmunization").append(liImmunization);
+                    }
+                })
+                .catch((err) => {
+                    // Error responses
+                    if (err.status) {
+                        console.log(err);
+                        console.log('Error', err.status);
+                    }
+                    // Errors
+                    if (err.data && err.data) {
+                        console.log('Error', err.data);
+                    }
+                });
+        });
+}
+
+function generateRandomAvatar() {
+    let arrAvatar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    let randomNum = Math.floor(Math.random() * arrAvatar.length);
+    let strPath = `app-assets/images/avatars/${arrAvatar[randomNum]}.png`;
+    $("#idAvatar").attr("src", strPath);
 }
